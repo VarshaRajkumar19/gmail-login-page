14dfa8d4cc866a3df9e7326cca4b697e7be06e34
VarshaRajkumar19 <varsharajkumar637@gmail.com>
2025-07-19 09:07:22 +0530

    Add files via upload

---
+++ Signinsuccess.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Signinsuccess.css';
+
+function Signinsuccess() {
+  return (
+    <div className="signinsuccess-container">
+      <div className="signinsuccess-card">
+        <p>You have successfully signed in to your account.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Signinsuccess;
